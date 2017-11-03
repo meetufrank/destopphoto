f3d886012dcc2dfb2ab03df4f8fe3c593515141e
wangjin <972270516@qq.com>
2017-11-03 22:18:37 +0800

    1

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -3,6 +3,7 @@ namespace app\home\controller;
 use app\home\model\IndexModel;
 use think\Db;
 use think\Cookie;
+use app\admin\model\MemberModel;
 class Index extends Base
 {
 	public function index(){
@@ -34,5 +35,53 @@ class Index extends Base
       return $this->fetch();
     }
     
+    
+    //上传生成专属码
+    public function getcode() {
+        
+         $member=new MemberModel();
+         $map=[
+             'openid'=> cookie::get('openid')
+                 ];
+          $count=$member->where($map)->getAllCount();
+          if($count){
+                   echo json_encode(['code'=>0,'msg'=>'您已生成专属码，无需再次生成']);
+                   exit;
+                }
+         
+         if(Cookie::has('imgurl')){
+              
+             //生成专属码
+             $num=time();
+             $num=substr($num,-6);
+             while(true){
+                 $map=[
+                   'openid'=> cookie::get('openid'),
+                    'unicount'=>$num
+                 ];
+                $count=$member->where($map)->getAllCount();
+                if($count){
+                    $num++;
+                }else{
+                    break;
+                }
+             }
+             $where=[
+             'openid'=> cookie::get('openid')
+                 ];
+           $data=[
+             'photo'=>cookie::get('imgurl'),
+              'unicount'=>$num
+            ]; 
+            $member->save($data,$where);
+            echo json_encode(['code'=>1,'msg'=>'上传成功']);
+                    exit;
+         }else{
+             echo json_encode(['code'=>0,'msg'=>'专属桌面异常，请重新生成专属桌面']);
+                   exit;
+         }
+         
+ 
+    }
 
 }
